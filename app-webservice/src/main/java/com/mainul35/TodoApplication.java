693a37aef0d99e6f63293666cb841223d1460394
Syed Mainul Hasan <mainuls18@gmail.com>
2019-10-19 23:56:33 +0600

    module imported

---
+++ app-webservice/src/main/java/com/mainul35/TodoApplication.java
@@ -0,0 +1,27 @@
+package com.mainul35;
+
+import com.mainul35.beans.DummyBeanDefinitions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:application.properties")
+public class TodoApplication implements CommandLineRunner {
+
+    @Autowired
+    DummyBeanDefinitions beanDefinitions;
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TodoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("App running");
+        beanDefinitions.print();
+    }
+}
