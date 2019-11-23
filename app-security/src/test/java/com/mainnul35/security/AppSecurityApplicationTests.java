8dbd6928663e0b1f4058f20d37378c6f4395b522
mainul35 <mainuls18@gmail.com>
2019-11-23 23:48:21 +0600

    oauth2 integration initialized

---
+++ app-security/src/test/java/com/mainnul35/security/AppSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainnul35.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
